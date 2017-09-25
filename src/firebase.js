0103dab0075425e6f505ad012ea6166ca8b846dd
Sasa Tatar <sasa.tatar@gmail.com>
2017-09-25 16:51:44 +0200

    fb keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAhWMYWhBG2UvsNMZ-qVLbaqVZ5aTzwsNU',
-  authDomain: 'hot-takes.firebaseapp.com',
-  databaseURL: 'https://hot-takes.firebaseio.com',
-  storageBucket: 'hot-takes.appspot.com',
-  messagingSenderId: '823384132241'
+  apiKey: "AIzaSyBOyr-ndwWrytSyyJNfe-UI0gep8WhgV1M",
+  authDomain: "hot-takes-2338a.firebaseapp.com",
+  databaseURL: "https://hot-takes-2338a.firebaseio.com",
+  projectId: "hot-takes-2338a",
+  storageBucket: "hot-takes-2338a.appspot.com",
+  messagingSenderId: "9113560273"
 };
 
 firebase.initializeApp(config);
